0ddaf226d28886c2d93d4aad781067b9ef1b656d
prati <prati@DESKTOP-QK9AO4D.home>
2017-09-05 16:49:39 -0700

    Optimized DFS With States Skipping.

--- src/homework/Homework.java
+++ src/homework/Homework.java
@@ -46,13 +46,13 @@ public class Homework {
         tree_count =  new int[dimension];
         col_lizard_count =  new int[dimension];
         Arrays.fill(col_lizard_count,0);
+        Arrays.fill(tree_count,0);
         ArrayList<Integer> temp =  new ArrayList<Integer>();
         for (int i = 0; i < dimension; i++) {
             String line = br.readLine();
             String[] row = line.split("");
             int[] num_row = new int[dimension];
             int type = 0;
-            tree_count[i] = 0;
             for(int k = 0; k< dimension; k++){
                 type = Integer.parseInt(row[k]);
                 num_row[k] = type;
@@ -74,9 +74,19 @@ public class Homework {
             grid_map[i] = num_row;
         }
 //        System.out.println("hello2");
+        for(int count : tree_count){
+            System.out.print(count + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < dimension; i++) {
+            System.out.print(i + " : ");
+            if(tree_map.containsKey(i)) tree_map.get(i).forEach(System.out::print);
+            System.out.println();
+        }
         NurseryGrid grid =  new NurseryGrid(0,dimension, grid_map);
         grid = solveGrid(grid,0,0);
         printSolution(grid);
+        System.out.println("Validity : " + validSolution(grid, total_lizard_count, dimension));
     }
     
     static NurseryGrid solveGrid(NurseryGrid grid,int position_i , int position_j){
@@ -101,9 +111,16 @@ public class Homework {
                 grid.grid_map[i][j] = 1;
                 grid.lizard_count += 1;
                 col_lizard_count[j] += 1;
-                if(shouldMoveAhead(i,j,grid.lizard_count))
-                    break;
                 next_cord = nextPosition(i,j);
+                if(!shouldMoveAhead(i,j,grid.lizard_count)){
+                    grid.grid_map[i][j] = 0;
+                    grid.lizard_count -= 1;
+                    col_lizard_count[j] -= 1;
+//                    System.out.println("Nikal diya BC I =" + i + " J = " + j);
+                    i = next_cord[0];
+                    j = next_cord[1];
+                    break;
+                }
 //                printTree(grid);
                 NurseryGrid new_g = solveGrid(grid,next_cord[0],next_cord[1]);
                 if(new_g.lizard_count == total_lizard_count){
@@ -113,6 +130,7 @@ public class Homework {
                     grid.grid_map[i][j] = 0;
                     grid.lizard_count -= 1;
                     col_lizard_count[j] -= 1;
+//                    System.out.println("Nikal diya BC I =" + i + " J = " + j);
                 }
             }
             else{
@@ -163,7 +181,7 @@ public class Homework {
        int[] cords = new int[2];
 //       if (col_lizard_count[j] >= tree_count[j] + 1){
 //           cords[0] = 0;
-//           cords[1] = ++j;
+//           cords[1] = ++j;    
 //       }
 //       else 
            if (i < dimension - 1){
@@ -183,16 +201,17 @@ public class Homework {
         int remaining_columns = dimension - col - 1 ;
         if(tree_count[col] > 0){       
             Integer[] col_trees = tree_map.get(col).toArray(new Integer[tree_count[col]]);
-            for(int col_index : col_trees){
-                if(col_index > col)
+            for(int row_index : col_trees){
+                if(row_index > row)
                     next_trees += 1;
             }
         }
         for (int i = col + 1; i < dimension; i++) {
            next_trees += tree_count[i]; 
         }
-        
-        return total_lizard_count - current_lizard_count >= next_trees + remaining_columns ? false : true ;
+//        System.out.println("Rem Lizards : " + (total_lizard_count - current_lizard_count));
+//        System.out.println("NT + NC : " + (next_trees + remaining_columns));
+        return (total_lizard_count - current_lizard_count) <= (next_trees + remaining_columns);
     }
     
     static void printSolution(NurseryGrid grid){
@@ -213,9 +232,29 @@ public class Homework {
     static void printTree(NurseryGrid grid){
         for(int n=0;n<dimension;n++){
                 for(int p=0;p<dimension;p++){
-                    System.out.print(grid.grid_map[n][p]);
+                    System.out.print(grid.grid_map[n][p] + "  ");
                 }
                 System.out.println("\n");
             }
+    }
+    
+    static boolean validSolution(NurseryGrid grid, int total_lizards, int dimension){
+        ArrayList<Integer> r = new ArrayList<Integer>() ;
+        ArrayList<Integer> c = new ArrayList<Integer>() ;
+//        System.out.println("Dimension : "+ dimension);
+        for(int n=0;n<dimension;n++){
+            for(int p=0;p<dimension;p++){
+                if(grid.grid_map[n][p] == 1)
+//                    System.out.println(" NP : " + n + " " + p);
+                    r.add(n);
+                    c.add(p);
+            }
+        }
+        for(int n=0; n<total_lizards; n++){
+            System.out.println(r.get(n) +" , "+ c.get(n));
+            if(canLizardBePlaced(grid, r.get(n), c.get(n)))
+                return false;
+        }
+        return true;
     }
 }
